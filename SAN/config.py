83fb245abee1dfce193e3c31c7d5feb4cf258e84
Hai Huynh <Nhut.H.Huynh@student.fh-Kiel.de>
2019-05-04 10:48:02 +0200

    add configuration file

---
+++ SAN/config.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 15 09:29:25 2019
+
+@author: hai huynh
+"""
+
+import tf_functions as fn
+
+"""
+Read data
+"""
+archi_fname = 'architecture.json'
+
+"""
+Preprocessing
+"""
+lo_wl = 350
+hi_wl = 800
+
+extrem_norm = 1e-3
+
+"""
+conv simple
+"""
+l_rate = 1e-3
+n_epochs = 100
+disp_step = 10
+
+"""
+training
+"""
+batch_size = 200
+
+"""
+Visualization
+"""
+style_ln = ['-', '--', '-.', '.']
